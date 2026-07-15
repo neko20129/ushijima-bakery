cd4c2de9ba046fd261dd76025ca0686dd3023dd9
neko20129 <minecraft.nekosan@outlook.jp>
2026-07-15 04:19:07 +0000

    window化

--- main.js
+++ main.js
@@ -1124,6 +1124,7 @@ function dataForSend() {
     console.log(uniqueId + ',' + bakeryName + ',' + level + ',' + money + ',' + profit + ',' + all + 'を送信した')
 }
 
+window.dataForSend = dataForSend();
 window.addEventListener('load', () => {
     dataForSend();
 });
